325c4a8468f8a315a0aefa9af896263f3b926992
ed.xyz <eddietang2314@gmail.com>
2023-04-19 20:35:07 -0400

    debug help and start apis

--- main.go
+++ main.go
@@ -1,34 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"webscrapper/pages"
+	"net/http"
 	"webscrapper/utils"
 )
 
 func main() {
 	c, e := utils.Init_colly()
+	utils.DebugLogin(c)
 	if e != nil {
 		log.Fatal("error initing colly scrapper")
 		log.Fatal(e)
 		return
 	}
-	// student := pages.ProfileData(c, 1)
-	// fmt.Printf("student: %v\n", student)
-	//pages.ProfileData(c, 1)
-	//pages.GimmeCourseCodes(c, 107604, "MP1")
-	// assignments := pages.AssignmentsDataForACourse(c, 107604, "MP2", "22000", "2", "US History I")
-	// for _, assignment := range assignments {
+	// // student := pages.ProfileData(c, 1)
+	// // fmt.Printf("student: %v\n", student)
+	// //pages.ProfileData(c, 1)
+	// //pages.GimmeCourseCodes(c, 107604, "MP1")
+	// // assignments := pages.AssignmentsDataForACourse(c, 107604, "MP2", "22000", "2", "US History I")
+	// // for _, assignment := range assignments {
+	// // 	fmt.Println(assignment)
+
+	// // }
+	// assignments_more := pages.ScheduleDataForACourse(c, 107604, "MP2", "22000", "2", "US History I")
+	// for _, assignment := range assignments_more {
 	// 	fmt.Println(assignment)
 
 	// }
-	assignments_more := pages.ScheduleDataForACourse(c, 107604, "MP2", "22000", "2", "US History I")
-	for _, assignment := range assignments_more {
-		fmt.Println(assignment)
-
-	}
-
+	// http.HandleFunc("/view/", makeHandler(viewHandler))
+	// http.HandleFunc("/edit/", makeHandler(editHandler))
+	// http.HandleFunc("/save/", makeHandler(saveHandler))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 	//pages.GradebookData(c, 107604, "MP1")
 
 }
--- utils/init.go
+++ utils/init.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"log"
 	"os"
+	"webscrapper/constants"
 
 	"github.com/gocolly/colly"
 	"github.com/joho/godotenv"
@@ -12,14 +13,6 @@ func Init_colly() (*colly.Collector, error) {
 
 	c := colly.NewCollector()
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error loading .env file")
-		return c, err
-	}
-
-	Login(c, os.Getenv("username"), os.Getenv("password"))
 
 	// c.OnRequest(func(r *colly.Request) {
 	// 	// Print the URL of the page being visited
@@ -37,5 +30,33 @@ func Init_colly() (*colly.Collector, error) {
 	// })
 
 	log.Println("finished initialization!")
+	return c, nil
+}
+
+func Login(c *colly.Collector, username string, password string) {
+	// authenticate
+	data := map[string]string{
+		constants.ConstantLinks["login"]["username"]: username,
+		constants.ConstantLinks["login"]["password"]: password,
+	}
+	err := c.Post(constants.ConstantLinks["login"]["url"], data)
+	if err != nil {
+		log.Println(err)
+		log.Println("Failed login")
+		// maybe an api call to login or smth and then deal with it like that return the error etc
+	}
+	log.Println("successful login!")
+
+}
+
+func DebugLogin(c *colly.Collector) (*colly.Collector, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+		return c, err
+	}
+
+	Login(c, os.Getenv("username"), os.Getenv("password"))
+
 	return c, nil
 }
