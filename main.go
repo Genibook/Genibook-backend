87e90fbd2e312136f25f638fd6f82d0ca9ec6647
ed.xyz <eddietang2314@gmail.com>
2023-04-21 19:03:57 -0400

    asfasdfsdaf

--- main.go
+++ main.go
@@ -19,12 +19,12 @@ func main() {
 
 	})
 
-	r.POST("/student/", api_v1.MakeHandler(api_v1.StudentHandlerV1))
-	r.POST("/schedule/", api_v1.MakeHandler(api_v1.ScheduleAssignmentHandlerV1))
-	r.POST("/assignments/", api_v1.MakeHandler(api_v1.AssignmentHandlerV1))
-	r.POST("/grades/", api_v1.MakeHandler(api_v1.GradesHandlerV1))
-	r.POST("/profile/", api_v1.MakeHandler(api_v1.ProfileHandlerV1))
-	r.POST("/login/", api_v1.MakeHandler(api_v1.LoginHandlerV1))
+	r.POST("/apiv1/student/", api_v1.MakeHandler(api_v1.StudentHandlerV1))
+	r.POST("/apiv1/schedule/", api_v1.MakeHandler(api_v1.ScheduleAssignmentHandlerV1))
+	r.POST("/apiv1/assignments/", api_v1.MakeHandler(api_v1.AssignmentHandlerV1))
+	r.POST("/apiv1/grades/", api_v1.MakeHandler(api_v1.GradesHandlerV1))
+	r.POST("/apiv1/profile/", api_v1.MakeHandler(api_v1.ProfileHandlerV1))
+	r.POST("/apiv1/login/", api_v1.MakeHandler(api_v1.LoginHandlerV1))
 
 	log.Fatal(r.Run(":6969"))
 	//log.Fatal(http.ListenAndServe(":6969", nil))
