0f187e4a5d6f6832a1b06727ca4697fa6a934c7e
ed.xyz <eddietang2314@gmail.com>
2023-10-04 17:15:52 -0400

    goofy error management

--- utils/error.go
+++ utils/error.go
@@ -11,6 +11,7 @@ import (
 func APIPrintSpecificError(prompt string, w http.ResponseWriter, theError error, code int) {
 	if theError != nil {
 		fmt.Printf("Error prompt: %v\n", prompt)
+		fmt.Printf("Error: %v\n", theError.Error())
 		http.Error(w, theError.Error(), code)
 		return
 	}
