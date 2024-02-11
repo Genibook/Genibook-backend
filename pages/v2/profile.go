0dcba5e2eaeeb42ff58aec8ae9812edee6438f8e
ed.xyz <eddietang2314@gmail.com>
2024-02-10 20:40:53 -0500

    asfasdf

--- pages/v2/profile.go
+++ pages/v2/profile.go
@@ -180,8 +180,7 @@ func StudentIds(c *colly.Collector, school string) ([]string, error) {
 	c.OnHTML("body", func(h *colly.HTMLElement) {
 		students := h.DOM.Find("div > div.selectStudent > div > ul > li.selectStudentItem")
 		//fmt.Println(h.DOM.Html())
-		log.Print(students.Length())
-		log.Println(" <- ID's length")
+		log.Println("There are " + string(rune(students.Length())) + " IDs found for this account.")
 		//fmt.Println(students.Length())
 		students.Each(func(i int, s *goquery.Selection) {
 			//fmt.Println(s.Html())
