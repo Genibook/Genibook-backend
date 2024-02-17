ecc1cc7b7ee9c0a79ad27300d4d1d85792db7366
Eddie <eddietang2314@gmail.com>
2024-02-16 20:06:43 -0500

    womp womp

--- go.mod
+++ go.mod
@@ -49,7 +49,6 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/heroku/x v0.1.0
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
--- main.go
+++ main.go
@@ -1,83 +1,57 @@
-// package main
-
-// import (
-// 	"log"
-// 	"net/http"
-// 	"os"
-// 	api "webscrapper/apis/v2"
-
-// 	"github.com/gin-gonic/gin"
-// )
-
-// func main() {
-// 	gin.SetMode(gin.ReleaseMode)
-
-// 	//r := gin.New()
-// 	// r.Use(gin.Recovery())
-// 	// r.Use(Logger())
-// 	r := gin.Default()
-
-// 	r.LoadHTMLFiles("./static/howGPA.html", "./static/pp.html")
-// 	//r.Use(api_v1.JsonLoggerMiddleware())
-
-// 	r.POST("/hi/", func(c *gin.Context) {
-// 		c.JSON(http.StatusOK, gin.H{
-// 			"message": "pong",
-// 		})
-
-// 	})
-
-// 	r.GET("/pp/", func(ctx *gin.Context) {
-// 		ctx.HTML(http.StatusOK, "pp.html", gin.H{})
-// 	})
-
-// 	r.GET("/howGPA/", func(ctx *gin.Context) {
-// 		ctx.HTML(http.StatusOK, "howGPA.html", gin.H{})
-// 	})
-
-// 	r.POST("/api/student/", api.MakeHandler(api.StudentHandlerV2))
-// 	r.POST("/api/assignments/", api.MakeHandler(api.AssignmentHandlerV2))
-// 	r.POST("/api/grades/", api.MakeHandler(api.GradesHandlerV2))
-// 	r.POST("/api/schedule/", api.MakeHandler(api.ScheduleAssignmentHandlerV2))
-// 	r.POST("/api/profile/", api.MakeHandler(api.ProfileHandlerV2))
-// 	r.POST("/api/login/", api.MakeHandler(api.LoginHandlerV2))
-// 	r.POST("/api/gpas/", api.MakeHandler(api.GPAshandlerV2))
-// 	r.POST("/api/gpas_his/", api.MakeHandler(api.GPAHistoryHandlerV2))
-// 	r.POST("/api/grade_of_students/", api.MakeHandler(api.StudentGradesHandlerV2))
-// 	r.POST("/api/mps/", api.MakeHandler(api.MpsHandlerV2))
-// 	r.POST("/api/ids/", api.MakeHandler(api.StudentIDHandlerV2))
-// 	r.GET("/api/transcript/", api.MakeHandler(api.TranscriptHandlerV2))
-
-// 	port := os.Getenv("PORT")
-// 	if port == "" {
-// 		port = "6969"
-// 	}
-
-//		log.Fatal(r.Run(":" + port))
-//	}
 package main
 
 import (
 	"log"
+	"net/http"
 	"os"
+	api "webscrapper/apis/v2"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	gin.SetMode(gin.ReleaseMode)
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+	//r := gin.New()
+	// r.Use(gin.Recovery())
+	// r.Use(Logger())
+	r := gin.Default()
 
-	router := gin.New()
-	router.Use(gin.Logger())
+	r.LoadHTMLFiles("./static/howGPA.html", "./static/pp.html")
+	//r.Use(api_v1.JsonLoggerMiddleware())
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"success": 1})
+	r.POST("/hi/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+
 	})
 
-	router.Run(":" + port)
+	r.GET("/pp/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "pp.html", gin.H{})
+	})
+
+	r.GET("/howGPA/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "howGPA.html", gin.H{})
+	})
+
+	r.POST("/api/student/", api.MakeHandler(api.StudentHandlerV2))
+	r.POST("/api/assignments/", api.MakeHandler(api.AssignmentHandlerV2))
+	r.POST("/api/grades/", api.MakeHandler(api.GradesHandlerV2))
+	r.POST("/api/schedule/", api.MakeHandler(api.ScheduleAssignmentHandlerV2))
+	r.POST("/api/profile/", api.MakeHandler(api.ProfileHandlerV2))
+	r.POST("/api/login/", api.MakeHandler(api.LoginHandlerV2))
+	r.POST("/api/gpas/", api.MakeHandler(api.GPAshandlerV2))
+	r.POST("/api/gpas_his/", api.MakeHandler(api.GPAHistoryHandlerV2))
+	r.POST("/api/grade_of_students/", api.MakeHandler(api.StudentGradesHandlerV2))
+	r.POST("/api/mps/", api.MakeHandler(api.MpsHandlerV2))
+	r.POST("/api/ids/", api.MakeHandler(api.StudentIDHandlerV2))
+	r.GET("/api/transcript/", api.MakeHandler(api.TranscriptHandlerV2))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "6969"
+	}
+
+	log.Fatal(r.Run(":" + port))
 }
